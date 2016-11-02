b35486fb89153767f77e5df8ff7c5a3958596be4
akhil12345 <akhilkannan0467@gmail.com>
2016-11-02 12:16:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,115 +1,53 @@
-var express = require('express');
+ var express = require('express');
  var morgan = require('morgan');
  var path = require('path');
  
  var app = express();
- app.use(morgan('combined'));
- 
- 
+  app.use(morgan('combined'));
   
-  var articles=
-  {
- -    articleOne:{`
- +    'article-One':{`
-      title:'article one | Akhil kannan',
-      heading: 'Article one',
-      date:'Sep 5 2016',
+  var articleOne = {
+    title:'article one | Akhil kannan'
+     heading: 'Article one'
+     date:'Sep 5 2016'
+     content: `<p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+    title:'article one | Akhil kannan',
+     heading: 'Article one',
+     date:'Sep 5 2016',
      content: `
      <p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-     </p>
-             
-     <p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-     </p>
-             
-      <p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-     </p>`
-  },
- -    articleTwo:
- -{
- +    'article-Two':{
-      title:'article two | Akhil kannan',
-      heading: 'Article Two',
-      date:'Sep 10 2016',
-      content: `
- -    <p>This is the content for my first article.This is the content for my second article.T
- +    <p>This is the content for my second article.This is the content for my second article.
-     </p>`
-      
-  },
- -   
- -    articleThree:
- -{
- +   'article-Three':{
-          title:'article three | Akhil kannan',
-      heading: 'Article Three',
-      date:'Sep 12 2016',
-      content: `
- -    <p>This is the content for my first article.This is the content for my second article.T
- +    <p>This is the content for my third article.This is the content for my third article.
-     </p>`},
+              </p>
+              
+              <p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+              </p>
+              
+              <p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+             </p>`
+    </p>`
       
   };
- 
- function createTemplate(data){
-     var title=data.title;
-     var date=data.date;
-     var heading=data.heading;
-     var content=data.content;
-     
-     
-     
- var htmlTemplate=`<html>
-     
-     <head>
-         <title> ${title}</title>
-         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <link href="/ui/style.css" rel="stylesheet" />
-     </head>
-     
-     
-     <body>
-     <div class="container">
-         <div>
-             <a href='/'>Home</a>
-         </div>
-         <hr/>
-         <h3>${heading}</h3>
-         <div>
-         ${date}
-         <div>
-             ${content}
-         </div>
-     </div>
-     </body>
- </html>
- `;
- return htmlTemplate;
- }
+  
  
  
  
  app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-  });
-  
-  
- -app.get('/article-one', function (req, res) {
- -res.send(createTemplate(articleOne))
- +app.get('/:articleName', function (req, res) {
- +    var articleName= req.params.articleName;
- +res.send(createTemplate(articles[articleName]))
-  });
-  
- -app.get('/article-two', function (req, res) {
- - res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
- -});
-  
- -app.get('/article-three', function (req, res) {
- -  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
- -});
-  
-  
-  app.get('/ui/style.css', function (req, res) {
+ });
+ 
+ 
+ app.get('/article-one', function (req, res) {
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ });
+ 
+ app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+ });
+ 
+ app.get('/article-three', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+ });
+ 
+ 
+ app.get('/ui/style.css', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
  });
  
