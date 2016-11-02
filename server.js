c866f861eea8d2c000cf5121f3efb0c095fffe90
akhil12345 <akhilkannan0467@gmail.com>
2016-11-02 12:22:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,71 +5,28 @@
  var app = express();
  app.use(morgan('combined'));
  
- var articleOne = {
-     title:'article one | Akhil kannan',
-     heading: 'Article one',
-     date:'Sep 5 2016',
-     content: `
-     <p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-             </p>
-             
-             <p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-             </p>
-             
-             <p>This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-    </p>`
-      
-  };
-  
- var htmlTemplate=`<html>
-     
-     <head>
-         <title> ${title}</title>
-         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <link href="/ui/style.css" rel="stylesheet" />
-     </head>
-     
-     
-     <body>
-     <div class="container">
-         <div>
-             <a href='/'>Home</a>
-         </div>
-         <hr/>
-         <h3>${heading}</h3>
-         <div>
-         ${date}
-         <div>
-             ${content}
-         </div>
-     </div>
-     </body>
- </html>
- `
-  
-  
-  
  app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
  });
- 
- 
- app.get('/article-one', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
- });
- 
+  
+  
+  app.get('/article-one', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send('Article-one will be served here');
+  });
+  
  app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+   res.send('Article-two will be served here');
  });
  
  app.get('/article-three', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+   res.send('Article-three will be served here');
  });
  
  
- app.get('/ui/style.css', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
- });
+  app.get('/ui/style.css', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  });
  
  app.get('/ui/madi.png', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
